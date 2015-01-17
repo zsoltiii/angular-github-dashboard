3452167a26625c689dc67f74fb43cb2871539ab4
Zsolt Szalai <zsoltiii@gmail.com>
2015-01-17 13:11:12 +0000

    angular config

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('zso02', [
+var zsoApp = angular.module('zsoApp', [
   'ngRoute',
-  'zso02Controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
+  'zsoControllers'
+]);
+
+zsoApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,4 +2,16 @@
 
 /* Controllers */
 
-var zso02Controllers = angular.module('zso02Controllers', []);
+var zsoControllers = angular.module('zsoControllers', ["ngResource"]);
+
+zsoControllers.controller('ZsoTwitterController', [$scope, $resource],
+    function($scope, $resource) {
+        var TwitterAPI = $resource("http://search.twitter.com/search.json",
+            { callback: "JSON_CALLBACK" },
+            { get: { method: "JSONP" }});
+
+        $scope.search = function() {
+            $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm });
+        };
+    }
+);
