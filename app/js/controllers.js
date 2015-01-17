3b35483c31abaecb43d91ec7b81caac84777d673
Zsolt Szalai <zsoltiii@gmail.com>
2015-01-17 14:34:40 +0000

    Successful no-auth github API queries

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,7 @@
 
 var zsoControllers = angular.module('zsoControllers', ["ngResource"]);
 
-zsoControllers.controller('ZsoTwitterController', [$scope, $resource],
+zsoControllers.controller('ZsoTwitterController', ['$scope', '$resource',
     function($scope, $resource) {
         var TwitterAPI = $resource("http://search.twitter.com/search.json",
             { callback: "JSON_CALLBACK" },
@@ -14,4 +14,43 @@ zsoControllers.controller('ZsoTwitterController', [$scope, $resource],
             $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm });
         };
     }
-);
+]);
+
+zsoControllers.controller('ZsoGithubSearchController', ['$scope', '$resource',
+    function($scope, $resource) {
+        var GithubSearchAPI = $resource(
+            'https://api.github.com/:query/:user/:repo/:spec',
+            {
+                'query': 'users',
+                'user': 'angular',
+                'repo': 'repos',
+                'spec': '',
+                'callback': 'JSON_CALLBACK',
+                'per_page': 10
+            }, {
+                'get': {
+                    'method': 'JSONP'
+                }
+            }
+        );
+
+        $scope.search = function() {
+            $scope.searchResult = GithubSearchAPI.get();
+        };
+    }
+]);
+
+zsoControllers.controller('ZsoGithubSearchControllerMock', ['$scope', '$resource',
+    function($scope, $resource) {
+        var GithubSearchAPI = $resource(
+            'js/github.json'
+        );
+
+        $scope.search = function() {
+            $scope.searchResult = GithubSearchAPI.get();
+        };
+
+        $scope.something = 'Hello!';
+    }
+]);
+
