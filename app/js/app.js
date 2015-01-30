df0311a39d57db4c5e6c8e61f0878f4022f92308
Zsolt Szalai <zsolt.szalai@fitchlearning.com>
2015-01-30 17:50:48 +0000

    Cleaning up a little

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,7 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-var zsoApp = angular.module('zsoApp', [
+var zsoGithubDashboard = angular.module('zsoGithubDashboard', [
     'ngRoute',
     'zsoServices',
     'zsoControllers'
 ]);
-
-zsoApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,32 +4,6 @@
 
 var zsoControllers = angular.module('zsoControllers', ["ngResource"]);
 
-zsoControllers.controller('ZsoGithubSearchController', ['$scope', '$resource',
-    function($scope, $resource) {
-        var GithubSearchAPI = $resource(
-            'https://api.github.com/:query/:user/:repo/:spec',
-            {
-                'query': 'users',
-                'user': 'angular',
-                'repo': 'repos',
-                'spec': '',
-                'callback': 'JSON_CALLBACK',
-                'per_page': 10
-            }, {
-                'get': {
-                    'method': 'JSONP',
-                    'cache': true
-                }
-            }
-        );
-
-        $scope.search = function() {
-            $scope.searchResult = GithubSearchAPI.get();
-            $scope.hideJumbotronWelcome = true;
-        };
-    }
-]);
-
 zsoControllers.controller('ZsoGithubSearchControllerMock', ['$scope', '$resource',
     function($scope, $resource) {
         var GithubSearchReposAPI = $resource(
