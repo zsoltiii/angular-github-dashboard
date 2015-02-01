e6f9dd8d83a81075870c5e75adf3ffea01e869a1
Zsolt Szalai <zsoltiii@gmail.com>
2015-02-01 22:27:42 +0000

    E2E tests running with Protractor

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -6,36 +6,43 @@ describe('zsoGithubDashboard', function() {
 
     beforeEach(function() {
         browser.get('app/');
-    })
+    });
 
     it('should render page title as Simple GitHub AngularJS Dashboard', function() {
         expect(browser.getTitle()).toBe('Simple GitHub AngularJS Dashboard');
     });
 
     it('should have navbar', function() {
-        var ele = by.id('navbar');
-        expect(browser.isElementPresent(ele)).toBe(true);
+        var ele = element(by.id('navbar'));
+        expect(ele.isDisplayed()).toBe(true);
     });
 
     it('should have hello message present', function () {
-        var ele = by.css('.jumbotron');
-        expect(browser.isElementPresent(ele)).toBe(true);
+        var ele = element(by.css('.jumbotron'));
+        expect(ele.isDisplayed()).toBe(true);
     });
 
     describe('Github API responses', function() {
 
         beforeEach(function() {
-            //element(by.input('searchTerm')).sendKeys('angular\n');
+            browser.get('app/');
+            browser.waitForAngular();
+            var inputEle = element(by.model('searchTerm'));
+            var searchButton = element(by.id('searchGithub'));
+            inputEle.sendKeys('angular');
+            searchButton.click();
         });
 
         it('search for angular should return with results', function() {
-            //expect(browser.isElementPresent())
+            element(by.css('.jumbotron')).then(function(ele) {
+                expect(ele.isDisplayed()).toBe(false);
+            })
         });
 
         it('should not have hello message present', function() {
-            //var ele = by.id('navbar');
-            //expect(browser.isElementPresent(ele)).toBe(true);
+            var elems = element.all(by.repeater('repo in searchReposResult.data'));
+            expect(elems.count()).toBeGreaterThan(0);
         });
     });
-
 });
+
