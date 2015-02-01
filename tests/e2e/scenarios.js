e0e128e4a3feb41ec198a52e348ebb3234282502
Zsolt Szalai <zsoltiii@gmail.com>
2015-02-01 18:24:34 +0000

    First steps with Protractor

--- tests/e2e/scenarios.js
+++ tests/e2e/scenarios.js
@@ -4,9 +4,38 @@
 
 describe('zsoGithubDashboard', function() {
 
-    browser.get('index.html');
+    beforeEach(function() {
+        browser.get('app/');
+    })
 
     it('should render page title as Simple GitHub AngularJS Dashboard', function() {
-        expect(true).toBe(true);
+        expect(browser.getTitle()).toBe('Simple GitHub AngularJS Dashboard');
     });
+
+    it('should have navbar', function() {
+        var ele = by.id('navbar');
+        expect(browser.isElementPresent(ele)).toBe(true);
+    });
+
+    it('should have hello message present', function () {
+        var ele = by.css('.jumbotron');
+        expect(browser.isElementPresent(ele)).toBe(true);
+    });
+
+    describe('Github API responses', function() {
+
+        beforeEach(function() {
+            //element(by.input('searchTerm')).sendKeys('angular\n');
+        });
+
+        it('search for angular should return with results', function() {
+            //expect(browser.isElementPresent())
+        });
+
+        it('should not have hello message present', function() {
+            //var ele = by.id('navbar');
+            //expect(browser.isElementPresent(ele)).toBe(true);
+        });
+    });
+
 });
